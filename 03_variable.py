33e710f7c8d18b46e4c6bd6cf9c3017bedf8973d
cirafranciscotomas421 <cirafranciscotomas421@gmail.com>
2022-06-03 16:08:25 -0500

    actualizado 03 junio 2022

---
+++ 03_variable.py
@@ -0,0 +1,10 @@
+print("variables y tipos de datos")
+var1=20
+var2=34.75
+var3="hola mundo"
+var4="True"
+print(var1,var2,var3,var4)
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
