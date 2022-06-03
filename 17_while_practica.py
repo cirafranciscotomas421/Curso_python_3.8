b56024fe708bb8760400b6dbcf482c6b4df01fb3
cirafranciscotomas421 <cirafranciscotomas421@gmail.com>
2022-06-03 16:51:56 -0500

    actualizado 03 junio 2022

---
+++ 17_while_practica.py
@@ -0,0 +1,16 @@
+"""print("multiplos de 5:")
+multiplo=5
+while multiplo<=50:
+    print(multiplo)
+    multiplo +=5
+print("fin ciclo")"""
+
+print("numeros pares")
+numero=int(input("intrudoce un numero par"))
+numero=2
+while numero<=25:
+    print(numero)
+    numero +=2
+print("fin ciclo")
+
+
