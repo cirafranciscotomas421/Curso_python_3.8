be7b72a576480a59af55e22a75386290f1f20e98
cirafranciscotomas421 <cirafranciscotomas421@gmail.com>
2022-06-03 16:52:50 -0500

    actualizado 03 junio 2022

---
+++ 18_paquete.py
@@ -0,0 +1,9 @@
+
+import paquete_funciones
+from paquete_funciones import suma
+
+print("suma: ",paquete_funciones.suma(10,25))
+print(paquete_funciones.saludo("uziel"))
+
+print("otra suma: ",suma(30,90))
+
