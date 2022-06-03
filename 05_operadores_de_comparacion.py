795e1831da5dd04874b418ee573be30eb26ca185
cirafranciscotomas421 <cirafranciscotomas421@gmail.com>
2022-06-03 16:12:41 -0500

    actualizado 03 junio 2022

---
+++ 05_operadores_de_comparacion.py
@@ -0,0 +1,9 @@
+operandoA=int(input("Anota un numero entero"))
+operandoB=int(input("Anota otro numero entero"))
+
+print(f"{operandoA} es igual a {operandoB}:  ", operandoA==operandoB)
+print(f"{operandoA} es igual distinto {operandoB}:  ", operandoA!=operandoB)
+print(f"{operandoA} es mayor a {operandoB}:  ", operandoA>operandoB)
+print(f"{operandoA} es menor a {operandoB}:  ", operandoA<operandoB)
+print(f"{operandoA} es mayor o igual a {operandoB}:", operandoA>=operandoB)
+print(f"{operandoA} es menor o igual a {operandoB}:  ", operandoA<=operandoB)
