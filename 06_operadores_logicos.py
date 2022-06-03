06be3ffbdc0ec3a038d7f2c827eb67c9e99fd08d
cirafranciscotomas421 <cirafranciscotomas421@gmail.com>
2022-06-03 16:29:46 -0500

    actualizado 03 junio 2022

---
+++ 06_operadores_logicos.py
@@ -0,0 +1,24 @@
+estado1=True
+etado2=False
+print("")
+
+proposicion1=False
+proposicion2=False
+proposicion3=False
+proposicion4=True
+proposicion5=True
+proposicion6=False
+proposicion7=True
+proposicion8=True
+print("tabla de verad de: and")
+print(proposicion1 and proposicion2)
+print(proposicion3 and proposicion4)
+print(proposicion5 and proposicion6)
+print(proposicion7 and proposicion8)
+print("tabla de verad de: or")
+print(proposicion1 or proposicion2)
+print(proposicion3 or proposicion4)
+print(proposicion5 or proposicion6)
+print(proposicion7 or proposicion8)
+#print(not proposicion1)
+#print(not proposicion2)
